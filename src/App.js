11f9033df5b3c3f398072eabe11970ac9b7c9d9d
Vijay-Kumar-Mern-Dev <degalavijaykumar77@gmail.com>
2023-02-01 21:36:59 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
+import Ad from './components/ad/Ad';
+import Latest from './components/latest/Latest';
+import Sports from './components/sports/Sports';
+import Diwali from './components/diwali/Diwali';
+import Movie from './components/movie/Movie';
+import Show from './components/shows/Show';
+import Special from './components/special/Special';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header/>
+      <Ad/>
+      <Latest/>
+      <Sports/>
+      <Diwali/>
+      <Movie/>
+      <Show/>
+      <Special/>
+      <Footer/>
+      </div>
   );
 }
 
